b19baff5f6550d44b092e9c5d70afbba6f04bacc
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:41:17 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-search - Trademark search and availability check - DPMA, EUIPO, WIPO databases. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
